609f8b49f33ac79509b9e694c0884d530a18bcde
flavien-hugs <geekactux@gmail.com>
2022-08-12 16:37:51 +0000

    create display labels function

--- core/main.py
+++ core/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from .constants import Color
+from .constants import Color, FontStyle
 
 
 class Calculator:
@@ -11,8 +11,29 @@ class Calculator:
         self.window.resizable(0, 0)
         self.window.title("unsta.inc calculator")
 
+        self.total_expression = "0"
+        self.current_expression = "0"
+
         self.display_frame = self.create_display_frame()
+        self.total_label = self.create_display_labels()
         self.buttons_frame = self.create_buttons_frame()
+
+    def create_display_labels(self):
+        total_label = tk.Label(
+            self.display_frame,
+            text=self.total_expression, anchor=tk.E,
+            bg=Color.COLOR_LIGHT_GRAY, fg=Color.COLOR_LABEL,
+            padx=24, font=FontStyle.SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill="both")
+
+        current_label = tk.Label(
+            self.display_frame,
+            text=self.current_expression, anchor=tk.E,
+            bg=Color.COLOR_LIGHT_GRAY, fg=Color.COLOR_LABEL,
+            padx=24, font=FontStyle.LARGE_FONT_STYLE)
+        current_label.pack(expand=True, fill="both")
+
+        return total_label, current_label
 
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=Color.COLOR_LIGHT_GRAY)
