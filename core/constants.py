1ba8d175c2485adbc473776da73eb5fd0c4ef9ab
flavien-hugs <geekactux@gmail.com>
2022-08-12 17:28:23 +0000

    add color red

--- core/constants.py
+++ core/constants.py
@@ -1,9 +1,12 @@
 class Color:
+    COLOR_RED = "red"
     COLOR_WITH = "#FFF"
     COLOR_LABEL = "#25265E"
+    COLOR_OFF_WITH = "#F8FAFF"
     COLOR_LIGHT_GRAY = "#F5F5F5"
 
 class FontStyle:
     SMALL_FONT_STYLE = ("Arial", 16)
     LARGE_FONT_STYLE = ("Arial", 40)
-    DIGIT_FONT_STYLE = ("Arial", 24, "bold")
+    DEFAULT_FONT_STYLE = ("Arial", 20, "bold")
+    DIGIT_FONT_STYLE = ("Arial", 20, "bold")
