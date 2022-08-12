33840498917a9cabb81dc493fba400207dc2054e
flavien-hugs <geekactux@gmail.com>
2022-08-12 16:58:05 +0000

    edit color white and font style

--- core/constants.py
+++ core/constants.py
@@ -1,7 +1,9 @@
 class Color:
+    COLOR_WITH = "#FFF"
     COLOR_LABEL = "#25265E"
     COLOR_LIGHT_GRAY = "#F5F5F5"
 
 class FontStyle:
     SMALL_FONT_STYLE = ("Arial", 16)
     LARGE_FONT_STYLE = ("Arial", 40)
+    DIGIT_FONT_STYLE = ("Arial", 24, "bold")
